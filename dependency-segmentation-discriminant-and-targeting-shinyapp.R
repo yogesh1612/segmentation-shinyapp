7002d19c15f2f9bbfdb6bae6f939ba4e83c4d7e6
Sudhir Voleti <sudhir_voleti@isb.edu>
2019-09-06 11:31:54 +0530

    Update dependency-segmentation-discriminant-and-targeting-shinyapp.R

--- dependency-segmentation-discriminant-and-targeting-shinyapp.R
+++ dependency-segmentation-discriminant-and-targeting-shinyapp.R
@@ -1,17 +1,14 @@
 if (!require("shiny")} {install.packages("shiny")}
-if (!require("devtools")} {install.packages("devtools")}
+if (!require("devtools")} {install.packages("devtools")}; library(devtools)
+if (!require("ggbiplot")} {install_github("ggbiplot","vqv")}
 if (!require("ggplot2")} {install.packages("ggplot2")}
 if (!require("dplyr")} {install.packages("dplyr")}
 # if (!require("plyr")} {install.packages("plyr")}
 if (!require("scales")} {install.packages("scales")}
-if (!require("grid")} {install.packages("grid")}
+if (!require("grid")} {install.packages("grid")}; library("grid")
 if (!require("cluster")} {install.packages("cluster")}
 if (!require("mclust")} {install.packages("mclust")}
 if (!require("MASS")} {install.packages("MASS")}
-
-# if (!require("ggbiplot")} {install_github("ggbiplot","vqv")}
-
- if (!require("ggplot2")} {install.packages("ggplot2")}
 if (!require("scales")} {install.packages("scales")}
 if (!require("gridExtra")} {install.packages("gridExtra")}
 
@@ -20,9 +17,7 @@ library("cluster")
 library("ggbiplot")
 library("mclust")
 library("MASS")
-
 library("ggplot2")
 library("scales")
 library("gridExtra")
-
-
+library("dplyr"")
